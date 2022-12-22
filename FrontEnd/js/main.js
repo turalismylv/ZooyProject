0d8ebdefcfe3fb6aecaf75b70017228e18635948
Eltun Ismayilov <eltunhi@code.edu.az>
2022-12-22 05:03:43 +0400

    FrontProject

---
+++ FrontEnd/js/main.js
@@ -0,0 +1,30 @@
+$('.single-item').slick();
+
+// counting days to new year
+const yearValue = document.querySelector("#year").innerHTML;
+const mountValue = document.querySelector("#mounth").innerHTML;
+const daysValuee = document.querySelector("#days").innerHTML;
+const countDownDateTime = new Date(yearValue, mountValue, daysValuee,);
+const daysValue = document.querySelector("#days");
+const hoursValue = document.querySelector("#hours");
+const minutesValue = document.querySelector("#minutes");
+const secondsValue = document.querySelector("#seconds");
+// run this function every 1000 ms or 1 second
+let x = setInterval(function () {
+ const dateTimeNow = new Date().getTime();
+ let difference = countDownDateTime - dateTimeNow;
+ // calculating time and assigning values
+ daysValue.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24)-30);
+ hoursValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+ );
+ minutesValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60)) / (1000 * 60)
+ );
+ secondsValue.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+ if (difference < 0) {
+ clearInterval(x);
+ }
+}, 1000);
+
+
